90d98d62e919f785853deec28c834d4ffd6b5c62
Rubens Vinicius Apolinario <vinicius.ap@live.com>
2020-10-19 01:58:37 -0300

    Adicionar arquivos de projeto.

---
+++ MinhasContas/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhasContas.Models;
+
+namespace MinhasContas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Register> registers = new List<Register>();
+            registers.Add(new Register("Conta de Luz", "Enel", 89.50, DateTime.Now, new DateTime(2020, 10, 25), new DateTime(2020, 10, 19)));
+            registers.Add(new Register("Conta de Agua", "Sabesp", 150.50, DateTime.Now, new DateTime(2020, 10, 25), new DateTime(2020, 10, 19)));
+            return View(registers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MinhasContas/Models/Register.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasContas.Models
+{
+    public class Register
+    {
+        public Register(string description, string title, double value, DateTime creationData, DateTime dueData, DateTime payData)
+        {
+            Description = description;
+            Title = title;
+            Value = value;
+            CreationData = creationData;
+            DueData = dueData;
+            PayData = payData;
+        }
+
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public double Value { get; set; }
+        public DateTime CreationData { get; set; }
+        public DateTime DueData { get; set; }
+        public DateTime PayData { get; set; }        
+
+    }
+}
